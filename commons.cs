82e96d678ab73223ce9cd4b8d098b7b370fd89be
ninhydrin <the.nocturne.of.daybreak@gmail.com>
2015-11-04 10:59:10 +0900

    ok net

---
+++ commons.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class commons :Photon.MonoBehaviour
+{
+	public bool[] ok;
+	public bool startF;
+	// Use this for initialization
+	void Start ()
+	{
+		ok = new bool[4];
+		startF = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	void OnPhotonSerializeView (PhotonStream stream, PhotonMessageInfo info)
+	{
+		if (stream.isWriting) {
+			for (int i = 0; i<4; i++)
+				stream.SendNext (ok [i]);
+			stream.SendNext (startF);
+			
+		} else {
+			for (int i = 0; i<4; i++)
+				ok [i] = (bool)stream.ReceiveNext ();
+			startF = (bool)stream.ReceiveNext ();
+		}
+	}
+}
