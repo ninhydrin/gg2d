80e44aee084659ec24592d8053d82572eaf1d30e
ninhydrin <the.nocturne.of.daybreak@gmail.com>
2015-11-05 10:00:52 +0900

    ok net

--- sava_head_HPLV.cs
+++ sava_head_HPLV.cs
@@ -1,21 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class sava_head_HPLV: MonoBehaviour
+public class sava_head_HPLV: Photon.MonoBehaviour
 {
 	private Vector3 screenPoint;
 	public GameObject myOb;
 	private bool canShow;
 	private GameObject HP_bar;
 	private GameObject LV_bar;
-
+	private RectTransform HPrt;
+	private RectTransform LVrt;
+	private bool cando;
 	float HPLenUnit;
+	int myObId;
 
 	void Start ()
 	{
 		HP_bar = transform.FindChild ("HP").gameObject;
 		LV_bar = transform.FindChild ("LV").gameObject;
+		HPrt = HP_bar.GetComponent<RectTransform> ();
+		LVrt = LV_bar.GetComponent<RectTransform> ();
 		canShow = false;
+
 		startinit ();
 	}
 
@@ -26,7 +32,7 @@ public class sava_head_HPLV: MonoBehaviour
 		if (myOb != null) {
 			screenPoint = Camera.main.WorldToScreenPoint (aa);
 			//screenPoint = Camera.main.WorldToScreenPoint (myOb.transform.position);
-			if (screenPoint.z > 30||screenPoint.z<0)
+			if (screenPoint.z > 30 || screenPoint.z < 0)
 				canShow = false;
 			else
 				canShow = true;
@@ -37,28 +43,47 @@ public class sava_head_HPLV: MonoBehaviour
 				HideBar ();
 			}
 		} else {
-			HideBar();
+			HideBar ();
 		}
 		HP_bar.GetComponent<RectTransform> ().sizeDelta = new Vector2 (HPLenUnit * myOb.GetComponent<Sava_controler> ().HP, HP_bar.GetComponent<RectTransform> ().sizeDelta.y);
 
 	}
-	public void init (GameObject target){
-		myOb = target;
-	}
-	void startinit(){
-		HPLenUnit = HP_bar.GetComponent<RectTransform> ().sizeDelta.x / (float)myOb.GetComponent<Sava_controler> ().maxHP;
 
+	public void init (GameObject target)
+	{
+		myOb = target;
+		myObId = target.GetComponent<PhotonView> ().viewID;
 	}
+
+	void startinit ()
+	{
+		if (!photonView.isMine)
+			myOb = PhotonView.Find (myObId).gameObject;
+		HPLenUnit = HP_bar.GetComponent<RectTransform> ().sizeDelta.x / (float)myOb.GetComponent<Sava_controler> ().maxHP;
+	}
+
 	void HideBar ()
 	{
 		GetComponent<UnityEngine.UI.Image> ().enabled = false;
 		HP_bar.GetComponent<UnityEngine.UI.Image> ().enabled = false;
 		LV_bar.GetComponent<UnityEngine.UI.Image> ().enabled = false;
 	}
-	void ShowBar(){
+
+	void ShowBar ()
+	{
 		GetComponent<UnityEngine.UI.Image> ().enabled = true;
 		HP_bar.GetComponent<UnityEngine.UI.Image> ().enabled = true;
 		LV_bar.GetComponent<UnityEngine.UI.Image> ().enabled = true;
+	}
+
+	void OnPhotonSerializeView (PhotonStream stream, PhotonMessageInfo info)
+	{
+		if (stream.isWriting) {
+			stream.SendNext (myObId);
+			
+		} else {
+			myObId = (int)stream.ReceiveNext ();
+		}
 	}
 }
 
